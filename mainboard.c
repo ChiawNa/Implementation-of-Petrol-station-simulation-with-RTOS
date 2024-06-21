278464511ce3b81723aee778210544acc7364384
ChiawNa <chiawnak0390@gmail.com>
2024-06-21 22:28:56 +0800

    Final code

--- mainboard.c
+++ mainboard.c
@@ -33,10 +33,10 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 int TotalPetrol = 50000;
-int pump1count = 0;
-int pump2count = 0;
-int pump3count = 0;
-int pump4count = 0;
+int pump1 = 0;
+int pump2 = 0;
+int pump3 = 0;
+int pump4 = 0;
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -45,99 +45,111 @@ int pump4count = 0;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-/* Definitions for Display */
-osThreadId_t DisplayHandle;
-const osThreadAttr_t Display_attributes = {
-  .name = "Display",
+/* Definitions for Pump1 */
+osThreadId_t Pump1Handle;
+const osThreadAttr_t Pump1_attributes = {
+  .name = "Pump1",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityNormal,
 };
+/* Definitions for Pump2 */
+osThreadId_t Pump2Handle;
+const osThreadAttr_t Pump2_attributes = {
+  .name = "Pump2",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Pump3 */
+osThreadId_t Pump3Handle;
+const osThreadAttr_t Pump3_attributes = {
+  .name = "Pump3",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Pump4 */
+osThreadId_t Pump4Handle;
+const osThreadAttr_t Pump4_attributes = {
+  .name = "Pump4",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for Sem1 */
+osSemaphoreId_t Sem1Handle;
+const osSemaphoreAttr_t Sem1_attributes = {
+  .name = "Sem1"
+};
+/* Definitions for Sem2 */
+osSemaphoreId_t Sem2Handle;
+const osSemaphoreAttr_t Sem2_attributes = {
+  .name = "Sem2"
+};
+/* Definitions for Sem3 */
+osSemaphoreId_t Sem3Handle;
+const osSemaphoreAttr_t Sem3_attributes = {
+  .name = "Sem3"
+};
+/* Definitions for Sem4 */
+osSemaphoreId_t Sem4Handle;
+const osSemaphoreAttr_t Sem4_attributes = {
+  .name = "Sem4"
+};
 /* USER CODE BEGIN PV */
-//static bool pump1flag = false;
-//static bool pump2flag = false;
-//static bool pump3flag = false;
-//static bool pump4flag = false;
-static bool pumpstopflag = false;
-uint32_t startTick = 0;
-uint32_t endTick = 0;
-int z=0;
+static bool pump1flag = false;
+static bool pump2flag = false;
+static bool pump3flag = false;
+static bool pump4flag = false;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-void StartDisplay(void *argument);
+void StartPump1(void *argument);
+void StartPump2(void *argument);
+void StartPump3(void *argument);
+void StartPump4(void *argument);
 
 /* USER CODE BEGIN PFP */
-
+void Task_action(char Message);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-    if (pumpstopflag == false && TotalPetrol > 0) {
-        if (GPIO_Pin == pump1_Pin) {
-
-        	z++;
-        	startTick = SysTick->VAL;
-
-        	pump1count++;
-
-        	z++;
-        	endTick = SysTick->VAL;
-
-        } else if (GPIO_Pin == pump2_Pin) {
-        	pump2count++;
-        } else if (GPIO_Pin == pump3_Pin) {
-        	pump3count++;
-        } else if (GPIO_Pin == pump4_Pin) {
-        	pump4count++;
-        }
-
-        TotalPetrol--;
-    }
-
-      else if (TotalPetrol == 0) { // Out of fuel
-            pumpstopflag = true;
-            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
-            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_SET);
-        }
-//        // Re-enable interrupts
-//        __enable_irq();
+void EXTI0_IRQHandler(void)
+{
+	if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_0) != RESET)
+	{
+		pump1flag = true;
+		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_0);
+	}
 }
 
-//void UpdatePumpCounts(char pump_id) {
-//    __disable_irq(); // Disable interrupts
-//    ITM_SendChar(pump_id);
-//    ITM_SendChar('\n');
-//
-//    switch (pump_id) {
-//        case '1':
-//            pump1count++;
-//            break;
-//        case '2':
-//            pump2count++;
-//            break;
-//        case '3':
-//            pump3count++;
-//            break;
-//        case '4':
-//            pump4count++;
-//            break;
-//    }
-//
-//    TotalPetrol--;
-//
-//    if (TotalPetrol == 0) { // Out of fuel
-//        pumpstopflag = true;
-//        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
-//        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_SET);
-//    }
-//
-//    __enable_irq(); // Re-enable interrupts
-//}
 
+void EXTI1_IRQHandler(void)
+{
+	if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_1) != RESET)
+	{
+		pump2flag = true;
+		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_1);
+	}
+}
 
+void EXTI4_IRQHandler(void)
+{
+	  if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_4) != RESET)
+	  {
+		  pump3flag = true;
+		  __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_4);
+	  }
+}
+
+void EXTI9_5_IRQHandler(void)
+{
+	  if(__HAL_GPIO_EXTI_GET_IT(GPIO_PIN_5) != RESET)
+	  {
+		  pump4flag = true;
+		  __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_5);
+	  }
+}
 /* USER CODE END 0 */
 
 /**
@@ -179,6 +191,19 @@ int main(void)
   /* add mutexes, ... */
   /* USER CODE END RTOS_MUTEX */
 
+  /* Create the semaphores(s) */
+  /* creation of Sem1 */
+  Sem1Handle = osSemaphoreNew(1, 1, &Sem1_attributes);
+
+  /* creation of Sem2 */
+  Sem2Handle = osSemaphoreNew(1, 1, &Sem2_attributes);
+
+  /* creation of Sem3 */
+  Sem3Handle = osSemaphoreNew(1, 1, &Sem3_attributes);
+
+  /* creation of Sem4 */
+  Sem4Handle = osSemaphoreNew(1, 1, &Sem4_attributes);
+
   /* USER CODE BEGIN RTOS_SEMAPHORES */
   /* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
@@ -192,8 +217,17 @@ int main(void)
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of Display */
-  DisplayHandle = osThreadNew(StartDisplay, NULL, &Display_attributes);
+  /* creation of Pump1 */
+  Pump1Handle = osThreadNew(StartPump1, NULL, &Pump1_attributes);
+
+  /* creation of Pump2 */
+  Pump2Handle = osThreadNew(StartPump2, NULL, &Pump2_attributes);
+
+  /* creation of Pump3 */
+  Pump3Handle = osThreadNew(StartPump3, NULL, &Pump3_attributes);
+
+  /* creation of Pump4 */
+  Pump4Handle = osThreadNew(StartPump4, NULL, &Pump4_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -230,7 +264,7 @@ void SystemClock_Config(void)
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -250,7 +284,7 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
@@ -276,23 +310,23 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2|GPIO_PIN_5, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : pump1_Pin pump2_Pin */
-  GPIO_InitStruct.Pin = pump1_Pin|pump2_Pin;
+  /*Configure GPIO pins : Pump1_Pin Pump2_Pin */
+  GPIO_InitStruct.Pin = Pump1_Pin|Pump2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PC2 PC5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_5;
+  /*Configure GPIO pin : PA2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : pump3_Pin pump4_Pin */
-  GPIO_InitStruct.Pin = pump3_Pin|pump4_Pin;
+  /*Configure GPIO pins : Pump3_Pin Pump4_Pin */
+  GPIO_InitStruct.Pin = Pump3_Pin|Pump4_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -315,16 +349,145 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+void Task_action(char Message)
+{
+	ITM_SendChar(Message);
+	ITM_SendChar('\n');
+}
 /* USER CODE END 4 */
 
-/* USER CODE BEGIN Header_StartDisplay */
+/* USER CODE BEGIN Header_StartPump1 */
 /**
-  * @brief  Function implementing the Display thread.
+  * @brief  Function implementing the Pump1 thread.
   * @param  argument: Not used
   * @retval None
   */
-/* USER CODE END Header_StartDisplay */
+/* USER CODE END Header_StartPump1 */
+void StartPump1(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+//	    osSemaphoreAcquire(Sem1Handle, osWaitForever);
+	    if(pump1flag == true && TotalPetrol > 0)
+	    {
+	    	Task_action('1');
+	    	pump1flag = false;
+
+	    	pump1 = pump1 + 1;
+	    	TotalPetrol = TotalPetrol - 1;
+	    }
+		else if(TotalPetrol == 0)											// out of fuel
+		{
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+		}
+//	    osSemaphoreRelease(Sem2Handle);
+//	    osThreadYield();
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartPump2 */
+/**
+* @brief Function implementing the Pump2 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartPump2 */
+void StartPump2(void *argument)
+{
+  /* USER CODE BEGIN StartPump2 */
+  /* Infinite loop */
+  for(;;)
+  {
+//	    osSemaphoreAcquire(Sem2Handle, osWaitForever);
+	    if(pump2flag == true && TotalPetrol > 0)
+	    {
+	    	Task_action('2');
+	    	pump2flag = false;
+
+	    	pump2 = pump2 + 1;
+	    	TotalPetrol = TotalPetrol - 1;
+	    }
+		else if(TotalPetrol == 0)											// out of fuel
+		{
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+		}
+//	    osSemaphoreRelease(Sem3Handle);
+//	    osThreadYield();
+  }
+  /* USER CODE END StartPump2 */
+}
+
+/* USER CODE BEGIN Header_StartPump3 */
+/**
+* @brief Function implementing the Pump3 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartPump3 */
+void StartPump3(void *argument)
+{
+  /* USER CODE BEGIN StartPump3 */
+  /* Infinite loop */
+  for(;;)
+  {
+//	    osSemaphoreAcquire(Sem3Handle, osWaitForever);
+	    if(pump3flag == true && TotalPetrol > 0)
+	    {
+	    	Task_action('3');
+	    	pump3flag = false;
+
+	    	pump3 = pump3 + 1;
+	    	TotalPetrol = TotalPetrol - 1;
+	    }
+		else if(TotalPetrol == 0)											// out of fuel
+		{
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+		}
+//	    osSemaphoreRelease(Sem4Handle);
+//	    osThreadYield();
+  }
+  /* USER CODE END StartPump3 */
+}
+
+/* USER CODE BEGIN Header_StartPump4 */
+/**
+* @brief Function implementing the Pump4 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartPump4 */
+void StartPump4(void *argument)
+{
+  /* USER CODE BEGIN StartPump4 */
+  /* Infinite loop */
+  for(;;)
+  {
+//	    osSemaphoreAcquire(Sem4Handle, osWaitForever);
+	    if(pump4flag == true && TotalPetrol > 0)
+	    {
+	    	Task_action('4');
+	    	pump4flag = false;
+
+	    	pump4 = pump4 + 1;
+	    	TotalPetrol = TotalPetrol - 1;
+	    }
+		else if(TotalPetrol == 0)											// out of fuel
+		{
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);
+		}
+//	    osSemaphoreRelease(Sem1Handle);
+//	    osThreadYield();
+  }
+  /* USER CODE END StartPump4 */
+}
+
 void StartDisplay(void *argument)
 {
   /* USER CODE BEGIN 5 */
@@ -339,7 +502,7 @@ void StartDisplay(void *argument)
 
       // Format the message
       unsigned int x=sprintf(message, "Total petrol : %d, pump 1 = %d, pump 2 = %d, pump 3 = %d, pump 4 = %d\n",
-              TotalPetrol, pump1count, pump2count, pump3count, pump4count);
+              TotalPetrol, pump1, pump2, pump3, pump4);
 
 		  for (int i = 0; i<x; ++i) {
 			  ITM_SendChar(message[i]);
